7c6e2ae0813d2f35e43288b414425c160180bb8d
chaya222 <poonam.19923@gmail.com>
2020-02-05 22:28:48 +0530

    Initial set up Added : NotesPreview Activity Added : AddNotes Activity Added : ShowNoteDetails Activity

---
+++ app/src/main/java/com/example/basicnoteapp/features/ShowNoteDetailsActivity.kt
@@ -0,0 +1,13 @@
+package com.example.basicnoteapp.features
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.basicnoteapp.R
+
+class ShowNoteDetailsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_show_note_details)
+    }
+}
